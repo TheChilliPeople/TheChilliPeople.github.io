681afd0debfb55ef312291c33d2487e7570ee6e5
Matt Cockerill <matt.cockerill1@gmail.com>
2020-10-04 18:38:17 +0100

    added js + css files and identified other media

---
+++ JS Scripts/js12.js
@@ -0,0 +1,51 @@
+(function (props, context) {
+    Core.utils.renderBootstrap({
+        elId: '8e91fcb1-535f-4e35-9b01-c34c40298927-bootstrap-container',
+        componentName: 'SubscribeForm1',
+        props: props,
+        context: context,
+        contextKey: 'context-bs-1',
+        radpack: false
+    });
+})({
+    "preset": "subscribe1",
+    "order": 0,
+    "id": "8e91fcb1-535f-4e35-9b01-c34c40298927",
+    "isInternalPage": false,
+    "env": "production",
+    "websiteId": "53ae2158-0c2d-448d-a6ce-3a9212531066",
+    "accountId": "ad39beb0-f156-11ea-81e5-3417ebe72601",
+    "hasNonCommercePlan": true,
+    "couponToggleHidden": false,
+    "sectionTitle": "Subscribe",
+    "staticContent": {
+        "defaultButtonLabel": "Sign Up",
+        "emailPlaceholderText": "Email Address",
+        "verificationText": "To let us know it's really you and that you'd like to receive emails from us, please click the link in the confirmation email we just sent you. You can unsubscribe from these emails at any time.",
+        "emailErrorMessage": "Please enter a valid email address."
+    },
+    "confirmationMessage": "Great, now please verify your email",
+    "couponConfirmationMessage": "Thanks so much for signing up!",
+    "description": "Sign up to be the first to hear about our new product range",
+    "couponDescription": "Get 10% off your first purchase when you sign up for our newsletter!",
+    "inputPlaceholder": "Email",
+    "subscribeButtonLabel": "Sign up",
+    "couponToggle": true,
+    "widgetId": "8e91fcb1-535f-4e35-9b01-c34c40298927",
+    "section": "default",
+    "category": "accent",
+    "locale": "en-GB",
+    "renderMode": "PUBLISH"
+}, {
+    "widgetId": "8e91fcb1-535f-4e35-9b01-c34c40298927",
+    "widgetType": "SUBSCRIBE",
+    "widgetPreset": "subscribe1",
+    "section": "default",
+    "category": "accent",
+    "fontSize": "medium",
+    "fontFamily": "alternate",
+    "group": "Section",
+    "groupType": "Default",
+    "websiteThemeOverrides": {},
+    "widgetThemeOverrides": {}
+});
---
+++ JS Scripts/js13.js
@@ -0,0 +1,75 @@
+(function (props, context) {
+    Core.utils.renderBootstrap({
+        elId: 'bs-8',
+        componentName: '@widget/GALLERY/bs-gallery8-Gallery',
+        props: props,
+        context: context,
+        contextKey: 'context-bs-1',
+        radpack: "@widget/GALLERY/bs-gallery8-Gallery"
+    });
+})({
+    "preset": "gallery8",
+    "id": "760a4744-3d56-4ab5-a3d2-8e487298b1ab",
+    "galleryImages": [{
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FOil%20in%20garden.jpeg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200810_210118.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200810_210114.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200810_184416.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200908_184925.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200908_185124.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200908_185131.jpg"
+        }
+    }, {
+        "image": {
+            "image": "\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002F53ae2158-0c2d-448d-a6ce-3a9212531066\u002FIMG_20200908_110312.jpg"
+        }
+    }],
+    "viewDevice": null,
+    "staticContent": {
+        "showMore": "Show More",
+        "demoCaption1": "Use captions to provide more information about your photos",
+        "demoCaption2": "Captions can prompt viewers to act. You can even insert a link",
+        "demoCaption3": "Use the caption to call out things the viewer may not notice",
+        "showLess": "Show Less"
+    },
+    "title": "A SNEAK PEAK",
+    "uniformWidthOrHeight": "uniformHeight",
+    "isInternalPage": false,
+    "widgetId": "760a4744-3d56-4ab5-a3d2-8e487298b1ab",
+    "section": "default",
+    "category": "accent",
+    "locale": "en-GB",
+    "renderMode": "PUBLISH"
+}, {
+    "widgetId": "760a4744-3d56-4ab5-a3d2-8e487298b1ab",
+    "widgetType": "GALLERY",
+    "widgetPreset": "gallery8",
+    "order": 1,
+    "section": "default",
+    "category": "accent",
+    "fontSize": "medium",
+    "fontFamily": "alternate",
+    "group": "Section",
+    "groupType": "Default",
+    "websiteThemeOverrides": {},
+    "widgetThemeOverrides": {}
+});
